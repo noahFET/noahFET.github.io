23168fecdf2cecbfb2e6dfa5fda2af2d4e04ef22
noahFET <noaheisner1124@gmail.com>
2026-03-03 16:43:07 -0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    // Code to run when the button is clicked
+    document.getElementById("myText").textContent = "Text changed!";
+});
